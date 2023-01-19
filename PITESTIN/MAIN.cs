ef591a31c0e9fbcb022a4dca46b554f802e7677d
Olleboalex <alexanderrasberg@gmail.com>
2023-01-19 11:06:17 +0100

    asdf

--- PITESTIN/MAIN.cs
+++ PITESTIN/MAIN.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Diagnostics;
 using Bigdecimal;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace piapprox
@@ -18,14 +16,9 @@ namespace piapprox
                 correctdigits++;
             }
         }
-        public static BigDecimal atanone(int n, int precision)
+        public static BigDecimal BD(int x, int precision)
         {
-            BigDecimal results = new BigDecimal(1, 0, precision);
-            for (int i = 1; i <= n; i++)
-            {
-                results += i % 2 == 0 ? (BigDecimal)1 / (2 * i + 1) : (BigDecimal)(-1) / (2 * i + 1);
-            }
-            return 4*results;
+            return new BigDecimal(x, 0, precision);
         }
         public static int[] LeibnizMethod(int n)
         {
@@ -52,11 +45,11 @@ namespace piapprox
             BigDecimal result = sqrt12;
             int[] results = new int[n];
 
-            BigDecimal Power3 = new BigDecimal(1, 0, sqrt12.precision);
+            BigDecimal Power3 = BD(1, sqrt12.precision);
             for (int i = 1; i <= n; i++)
             {
-                Power3 *= new BigDecimal(3, 0, sqrt12.precision);
-                result += i % 2 == 0 ? sqrt12 / (new BigDecimal(2 * i + 1, 0, sqrt12.precision) * Power3) : -sqrt12 / (new BigDecimal(2 * i + 1, 0, sqrt12.precision) * Power3);
+                Power3 *= BD(3, sqrt12.precision);
+                result += i % 2 == 0 ? sqrt12 / (BD(2 * i + 1, sqrt12.precision) * Power3) : -sqrt12 / (BD(2 * i + 1, sqrt12.precision) * Power3);
                 results[i - 1] = comparetotext(result.toString(), PI);
             }
             return results;
@@ -64,28 +57,24 @@ namespace piapprox
         public static int[] BaileyMethod(int n)
         {
             const int precision = 1500;
-            BigDecimal result = new BigDecimal(33,-1,precision) - new BigDecimal(1,0,precision)/ 6;
+            BigDecimal result = new BigDecimal(33,-1,precision) - BD(1, precision)/ 6;
             int[] results = new int[n];
             results[0] = comparetotext(result.toString(), PI);
-            BigDecimal sixteen = new BigDecimal(1, 0, precision) / 16;
+            BigDecimal sixteen = BD(1, precision) / 16;
             for (int i = 1; i < n; i++)
             {
-                result += sixteen * (new BigDecimal(4, 0, precision) / (8 * i + 1) - new BigDecimal(2, 0, precision) / (8 * i + 4) - new BigDecimal(1, 0, precision) / (8 * i + 5) - new BigDecimal(1, 0, precision) / (8 * i + 6));
+                result += sixteen * (BD(4, precision) / (8 * i + 1) - BD(2, precision) / (8 * i + 4) - BD(1, precision) / (8 * i + 5) - BD(1,precision) / (8 * i + 6));
                 sixteen /= 16;
                 results[i] = comparetotext(result.toString(), PI);
             }
             return results;
         }
-        public static BigDecimal BD(int x, int precision)
-        {
-            return new BigDecimal(x, 0, precision);
-        }
         public static int[] BellardMethod(int n)
         {
             const int precision = 3300;
             BigDecimal onedivsixfour = new BigDecimal(15625,-6,precision);
-            BigDecimal result = new BigDecimal(0, 0, precision);
-            BigDecimal onedivtusentjugofyra = new BigDecimal(1, 0, precision);
+            BigDecimal result = BD(0, precision);
+            BigDecimal onedivtusentjugofyra = BD(1, precision);
             int[] results = new int[n];
             
             for (int i = 0; i < n; i++)
@@ -96,50 +85,6 @@ namespace piapprox
             }
             return results;
         }
-
-
-        public static int[] runtest(Func<int,int, BigDecimal> fun, int tests)
-        {
-
-            int[] results = new int[tests];
-            int precision = 15;
-            for (int i = 1; i <= tests; i++)
-            {
-                BigDecimal result = fun(i,precision);
-                int decimals = comparetotext(result.toString(), PI);
-                if (decimals >= precision)
-                {
-                    precision *= 2;
-                    i--;
-                    continue;
-                }
-                results[i - 1] = decimals;
-            }
-            return results;
-        }
-        public static int[] runtest(Func<int,int, BigDecimal> fun, int tests, int precision)
-        {
-            int[] results = new int[tests];
-            Parallel.For(1, tests + 1, i =>
-            {
-                results[i - 1] = comparetotext(fun(i,precision).toString(), PI);
-            });
-            return results;
-        }
-        // results vid itteration 1 är index 0
-        /*static void writeresults(int[] results, string address)
-        {
-            using (var workbook = new XLWorkbook())
-            {
-                var worksheet = workbook.Worksheets.Add("Data");
-                for (int i = 0; i < results.Length; i++)
-                {
-                    worksheet.Cell("A" + (i + 1)).Value = i + 1;
-                    worksheet.Cell("B" + (i + 1)).Value = results[i];
-                    workbook.SaveAs(address);
-                }
-            }
-        }*/
 
         static string PI;
         public static BigDecimal sqrt12;
